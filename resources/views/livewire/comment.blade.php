38921a54c0953aa03c1a0f6e7f564b00f5e9d811
Abdelghani <b.abdellghani@gmail.com>
2024-04-09 15:44:39 +0000

    Add Comment Reply View

--- resources/views/livewire/comment.blade.php
+++ resources/views/livewire/comment.blade.php
@@ -1,4 +1,4 @@
-<div>
+<div x-data="{ isReplying: false }">
     <article class="my-6 text-base bg-white rounded-lg">
         <footer class="flex items-center justify-between mb-6">
             <div class="flex items-center">
@@ -21,7 +21,8 @@
         {{-- Reply, Edit, Delete Section --}}
         <div class="flex items-center mt-4 space-x-4">
             @if (!$comment->parent_id)
-                <button type="button" class="flex items-center text-sm text-gray-500 hover:underline">
+                <button @click="isReplying=!isReplying" type="button"
+                    class="flex items-center text-sm text-gray-500 hover:underline">
                     <svg aria-hidden="true" class="mr-1 w-4 h-4" fill="none" stroke="currentColor"
                         viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -51,28 +52,29 @@
                 </svg>
                 Delete
             </button>
+        </div>
+    </article>
+
+    {{-- Reply Form --}}
+    <form class="mb-6 mt-6 ml-6" x-show="isReplying" x-transition>
+        <div class="py-2 mb-4">
+            <label for="comment" class="sr-only">Your comment</label>
+            <textarea id="comment"
+                class="shadow-sm block rounded-md w-full border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500"
+                placeholder="Write a comment..."></textarea>
 
         </div>
+        <button type="submit"
+            class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-primary-200 hover:bg-blue-800">
+            Reply
+        </button>
 
-        {{-- <form class="mb-6 mt-6 ml-6" wire:submit="replyComment">
-            <div class="py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg border border-gray-200">
-                <label for="comment" class="sr-only">Your comment</label>
-                <textarea id="comment" style="resize: none"
-                    class="px-0 w-full text-sm text-gray-900 border-0 focus:ring-0 focus:outline-none" placeholder="Write a comment..."></textarea>
-
-            </div>
-            <button type="submit"
-                class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-primary-200 hover:bg-blue-800">
-                Reply
-            </button>
-
-            <button
-                class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center border-gray-300 text-gray-700 bg-white hover:bg-gray-50 rounded-lg">
-                Cancel
-            </button>
-        </form> --}}
-
-    </article>
+        <button @click="isReplying=false"
+            type="button"
+            class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center border-gray-300 text-gray-700 bg-white hover:bg-gray-50 rounded-lg">
+            Cancel
+        </button>
+    </form>
 
     {{-- Replies --}}
     <div class="ml-10 mt-10">
